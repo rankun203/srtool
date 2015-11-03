a89f1a2ba9ca81d8c19f428973a0df0fa336e38d
rankun203 <rankun203@gmail.com>
2015-11-04 01:45:21 +0800

    UPDATE: program to process files

--- format.js
+++ format.js
@@ -3,6 +3,9 @@
  * @author rankun203
  */
 
+var fs = require('fs');
+var readline = require('readline');
+var stream = require('stream');
 var SRT = require('./srt/SRTnode');
 
 var s1 = new SRT(1, '00:00:14,460', '00:00:20,170', 'SUGARfx Subtitles Pro is capable of displaying hundreds subtitles.');
@@ -10,3 +13,27 @@ var s2 = new SRT(1, '00:00:14,460', '00:00:20,170', 'SUGARfx Subtitles Pro is ca
 
 console.log(s1.srt());
 console.log(s2.srt());
+
+
+var instream = fs.createReadStream('./data/test_sub.txt');
+var outfile = fs.openSync('./data/test_sub_out.txt', 'w');
+var regTime = /^\d.*/;
+var regEnglish = /^[a-zA-Z].*/;
+var regChinese = /^[^\w^\s].*/;
+
+var rl = readline.createInterface({
+  input: instream,
+  terminal: false
+});
+
+rl.on('line', function (line) {
+
+  if (regTime.test(line))
+    console.log('TIME: ', line);
+  else if (regEnglish.test(line))
+    console.log('ENG', line);
+  else if (regChinese.test(line))
+    console.log('CHN', line);
+
+  fs.write(outfile, line + '\n');
+});
---
+++ srt/SRTparser.js
@@ -0,0 +1,16 @@
+/**
+ * Created on 11/4/15.
+ * @author rankun203
+ */
+
+function Parser(format) {
+  this.format = format || null;
+}
+
+Parser.prototype.setFormat = function () {
+
+};
+
+Parser.prototype
+
+module.exports = Parser;
